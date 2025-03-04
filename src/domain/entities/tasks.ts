a9dfbc9383099a01c20a4cff7700a3611d54ad7a
maxpdr96 <maxpdr96@gmail.com>
2025-03-04 14:08:08 -0300

    Exercicio 01

---
+++ src/domain/entities/tasks.ts
@@ -0,0 +1,12 @@
+import { IProject } from '../interfaces/project.interface';
+import { ITask } from '../interfaces/task.interface';
+import { IUser } from '../interfaces/user.interface';
+
+export class Tasks implements ITask {
+    id: number;
+    name: string;
+    status: 'pending' | 'completed';
+    project: IProject;
+    user: IUser;
+
+}
