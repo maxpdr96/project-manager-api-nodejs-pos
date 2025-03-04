4155736cd840852add71b2775ebb3a88ef586bec
maxpdr96 <maxpdr96@gmail.com>
2025-03-04 14:19:59 -0300

    Ajuste nome task class

--- src/domain/entities/task.ts
+++ src/domain/entities/task.ts
@@ -2,7 +2,7 @@ import { IProject } from '../interfaces/project.interface';
 import { ITask } from '../interfaces/task.interface';
 import { IUser } from '../interfaces/user.interface';
 
-export class Tasks implements ITask {
+export class Task implements ITask {
   id: number;
   name: string;
   status: 'pending' | 'completed';
