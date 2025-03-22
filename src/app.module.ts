d49a91cdcaa151649cb8b49204afbd6f38a3b507
maxpdr96 <maxpdr96@gmail.com>
2025-03-22 15:42:06 -0300

    Exercicio 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DomainModule } from './domain/domain.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { DomainModule } from './domain/domain.module';
 import { GatewaysModule } from './gateways/gateways.module';
-import { GetProjectByIdService } from './domain/use-cases/projects/get-project-by-id.service';
-import { CreateProjectService } from './domain/use-cases/projects/create-project.service';
-import { GetAllTasksService } from './domain/use-cases/tasks/get-all-tasks.service';
-import { GetTaskByIdService } from './domain/use-cases/tasks/get-task-by-id.service';
-import { CreateTaskService } from './domain/use-cases/tasks/create-task.service';
-import { UpdateTaskService } from './domain/use-cases/tasks/update-task.service';
-import { CreateUserService } from './domain/use-cases/users/create-user.service';
-import { GetUserByIdService } from './domain/use-cases/users/get-user-by-id.service';
-import { DatabaseModule } from './infrastructure/database/database.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuardService } from './gateways/guards/auth-guard.service';
 
 @Module({
-  imports: [DomainModule, InfrastructureModule, GatewaysModule, DatabaseModule],
+  imports: [InfrastructureModule, DomainModule, GatewaysModule],
   controllers: [AppController],
   providers: [
     AppService,
-    GetProjectByIdService,
-    CreateProjectService,
-    GetAllTasksService,
-    GetTaskByIdService,
-    CreateTaskService,
-    UpdateTaskService,
-    CreateUserService,
-    GetUserByIdService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuardService,
+    },
   ],
 })
 export class AppModule {}
--- src/gateways/controllers/tasks/tasks.controller.ts
+++ src/gateways/controllers/tasks/tasks.controller.ts
@@ -5,6 +5,7 @@ import {
   NotFoundException,
   Param,
   Post,
+  Req,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { CreateTaskService } from 'src/domain/use-cases/tasks/create-task.service';
@@ -12,7 +13,6 @@ import { GetAllTasksService } from 'src/domain/use-cases/tasks/get-all-tasks.ser
 import { GetTaskByIdService } from 'src/domain/use-cases/tasks/get-task-by-id.service';
 import { CreateTaskDto } from './dtos/create-task.dto';
 
-const userId = 1;
 @Controller('tasks')
 export class TasksController {
   constructor(
@@ -20,33 +20,35 @@ export class TasksController {
     private readonly getTaskByIdUseCase: GetTaskByIdService,
     private readonly createTaskUseCase: CreateTaskService,
   ) {}
-
   @Get()
-  async findAll() {
+  async findAll(@Req() request) {
     try {
-      return await this.getAllTasksUseCase.execute({ userId });
+      const loggedUser = request.user;
+      return await this.getAllTasksUseCase.execute({
+        userId: loggedUser.sub,
+      });
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
-
   @Get(':id')
-  async findOne(@Param('id') id: number) {
+  async findOne(@Req() request, @Param('id') id: number) {
     try {
+      const loggedUser = request.user;
       return await this.getTaskByIdUseCase.execute({
-        userId,
+        userId: loggedUser.sub,
         taskId: id,
       });
     } catch (error) {
       throw new NotFoundException(error.message);
     }
   }
-
   @Post()
-  async create(@Body() createTaskDto: CreateTaskDto) {
+  async create(@Req() request, @Body() createTaskDto: CreateTaskDto) {
     try {
+      const loggedUser = request.user;
       return await this.createTaskUseCase.execute({
-        userId,
+        userId: loggedUser.sub,
         task: createTaskDto,
       });
     } catch (error) {
